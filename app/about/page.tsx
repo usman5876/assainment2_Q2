9235c92b8b05335715ca57ea0ef46a02b522941b
Muhammad Usman <60290215+usman5876@users.noreply.github.com>
2023-11-26 11:32:07 +0500

    updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -42,7 +42,7 @@ export default function about(){
             <section className="about-us">
                 <div className="row">
                 <div className="about-col">
-                    <h1>We are the world's largest University</h1>
+                    <h1>We are the world&apos;s largest University</h1>
                     <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
                     cumque in accusantium molestias maiores fuga vitae eos, ducimus
